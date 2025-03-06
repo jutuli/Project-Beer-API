fea48cfc99ccc7ab93f3aaafc4d90e4d8c3f887b
Julia T <juliaturkali@gmail.com>
2025-03-06 11:06:49 +0100

    Enhance Footer component with navigation link and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-    return ( <footer></footer> );
-}
- 
+  return (
+    <footer className="flex h-20 w-full items-center justify-center bg-amber-400">
+      <Link to="/">
+        <p className="size-15 rounded-full bg-slate-100 p-3 text-4xl">🍻</p>
+      </Link>
+    </footer>
+  );
+};
+
 export default Footer;

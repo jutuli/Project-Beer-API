aef157183f981b7345b30a879f5f84dd1cb6afd9
Julia T <juliaturkali@gmail.com>
2025-03-06 12:06:35 +0100

    Refactor routing for Random Beer and enhance Home component with links and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   createBrowserRouter,
   Route,
   createRoutesFromElements,
   RouterProvider,
-} from 'react-router-dom';
-import Layout from './Layout/Layout';
-import Home from './pages/Home';
-import BeerDetail from './pages/Beerdetail';
-import BeerList from './pages/Beerlist';
+} from "react-router-dom";
+import Layout from "./Layout/Layout";
+import Home from "./pages/Home";
+import BeerDetail from "./pages/Beerdetail";
+import BeerList from "./pages/Beerlist";
+import RandomBeer from "./pages/RandomBeer";
 
-function App() {
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-    <Route index element={<Home/>}/>
-    <Route path="/beers" element={<BeerList/>}/>
-    <Route path="/beers:id" element={<BeerDetail/>}/>
-    </Route>
-  ))
-  return( 
-   <>
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/beers" element={<BeerList />} />
+          <Route path="/beers/:beerId" element={<BeerDetail />} />
+          <Route path="beers/random-beer" element={<RandomBeer />} />
+        </Route>
+      </>,
+    ),
+  );
+
+  return (
+    <>
       <RouterProvider router={router} />
-    </>)
-}
+    </>
+  );
+};
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,46 @@
+import all_beers from "../assets/img/all_beers.png";
+import random_beer from "../assets/img/random_beer.png";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-    return ( <>Home</> );
-}
- 
+  return (
+    <>
+      <section className="mx-20 my-10 flex flex-col items-center justify-center gap-20">
+        <div className="flex w-full flex-col">
+          <Link to="/beers">
+            <img
+              src={all_beers}
+              alt="Assortion of Beers"
+              className="w-full cursor-pointer"
+            />
+            <h2 className="bg-amber-400 px-6 py-4 text-5xl font-bold text-white">
+              All Beers
+            </h2>
+          </Link>
+          <p className="py-4 text-3xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, a,
+            eleifend vitae varius venenatis.
+          </p>
+        </div>
+        <div className="w-full">
+          <Link to={`/beers/random-beer`}>
+            <img
+              src={random_beer}
+              alt="Random Beer"
+              className="w-full cursor-pointer"
+            />
+            <h2 className="bg-amber-400 px-6 py-4 text-5xl font-bold text-white">
+              Random Beer
+            </h2>
+          </Link>
+          <p className="py-4 text-3xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo, a,
+            eleifend vitae varius venenatis.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
 export default Home;
